8bc7e16e9c0f384baf86a2f0033955b8c5308cba
juanalsan <118403734+juanalsan@users.noreply.github.com>
2022-12-13 19:41:31 +0100

    Update Ceros.hs
    nuevo descubrir que sustituye a ceros

--- minesweeper/Ceros.hs
+++ minesweeper/Ceros.hs
@@ -1,53 +1,18 @@
 module Ceros
-( buscarCerosTablero
-, limpiarTableroFeliz
-, limpiarCerosFront
-, limpiarCerosTablero
-, limpiarCeros
-, flipTresMedioDcha
-, repetirN
-, listIncidencia
+(nuevoDescubrirLista
+,nuevoDescubrir
 ) where
 import Datos
 import Descubrir
 
-buscarCerosTablero :: (Int,Int) -> TableroFront -> [(Int,Int)]
-buscarCerosTablero _ [[]] = []
-buscarCerosTablero (n,m) tablero
-   |tablero!!0 == []                     = []
-   |a == length(tablero)                 = []
-   |b >= length(tablero!!0)              = (buscarCerosTablero (a+1,0) tablero)
-   |(tablero!!a)!!b == (Desc(NoMina[0])) = (a,b) : (buscarCerosTablero (a,b+1) tablero)
-   |otherwise = buscarCerosTablero (a,b+1) tablero
-   where a = fst (n,m)
-         b = snd (n,m)
-
-flipTresMedioDcha :: (a -> b -> c -> d) -> a -> c -> b -> d
-flipTresMedioDcha f x z y = f x y z
-
--- la funcion que busca ceros despues de cada loop es pasar 
--- map (flipTresMedioDcha descubrirCeros (0,0) tablero2) (buscarCerosTablero (0,0) tablero2)
-
-limpiarTableroFeliz :: TableroFront -> TableroFront
-limpiarTableroFeliz  tablero = repetirN limpiarCerosFront tablero (length tablero)
-
-limpiarCerosFront :: TableroFront -> TableroFront
-limpiarCerosFront tablero   =  f listaDeIncidencias
-   where listaDeIncidencias = map listIncidencia (buscarCerosTablero (0,0) tablero)
-         f                  = limpiarCerosTablero tablero
-
-repetirN :: (a -> a) -> a -> Int -> a
-repetirN f x n  = iterate f x !! n
 
 
-limpiarCerosTablero :: TableroFront -> [[(Int,Int)]] -> TableroFront
-limpiarCerosTablero tablero []     = tablero
-limpiarCerosTablero tablero (x:xs) = limpiarCerosTablero (limpiarCeros tablero x) xs 
+nuevoDescubrirLista :: TableroFront -> [(Int,Int)] -> TableroFront 
+nuevoDescubrirLista tablero [] = tablero
+nuevoDescubrirLista tablero (x:xs) = nuevoDescubrirLista (nuevoDescubrir tablero x) xs
 
-
-limpiarCeros :: TableroFront -> [(Int,Int)] -> TableroFront
-limpiarCeros tablero []     = tablero
-limpiarCeros tablero (x:xs) = limpiarCeros (descubrir 0 x tablero) xs
-
-listIncidencia :: (Int,Int) -> [(Int,Int)]
-listIncidencia (n,m) = [(n+x,m+y) | x <- [-1,0,1], y <- [-1,0,1]]  
+nuevoDescubrir :: TableroFront -> (Int,Int) -> TableroFront
+nuevoDescubrir tablero (fil,col)
+   |(tablero!!fil)!!col == (NoDesc(NoMina[0])) = nuevoDescubrirLista (descubrir 0 (fil,col) tablero) lista
+   |otherwise                 = descubrir 0 (fil,col) tablero
+   where lista = [(fil,col-1)]++[(fil,col+1)]++[(fil+y, col+x)| x <- [-1,0,1],y <- [-1,1]] 
